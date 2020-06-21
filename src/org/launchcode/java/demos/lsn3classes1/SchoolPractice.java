d9bb94cdae317c247a8ec2fec1ffb0531cf1ec97
EricSoto <ericsoto926@gmail.com>
2020-06-20 22:47:44 -0400

    Currently working through Class 3 exercises

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -3,5 +3,11 @@ package org.launchcode.java.demos.lsn3classes1;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+
+        Student Eric = new Student();
+        Eric.setGpa(4.0);
+        Eric.setName("Eric Soto Rivera");
+        Eric.setNumberOfCredits(1);
+
     }
 }
